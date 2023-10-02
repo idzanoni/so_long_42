f17f3dd3e3c20ce71629f60d54ee4fced2742701
Izabel Zanoni <izanoni@c1r3p1.42sp.org.br>
2023-10-02 12:56:16 -0300

    antes de mexer em eventos

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: izanoni <izanoni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 20:41:45 by izanoni           #+#    #+#             */
-/*   Updated: 2023/09/26 19:06:18 by izanoni          ###   ########.fr       */
+/*   Updated: 2023/10/02 12:11:19 by izanoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,39 +30,28 @@
 # define BLUEY_U "textures/bluey_up.xpm"
 # define BLUEY_L "textures/bluey_left.xpm"
 # define BLUEY_R "textures/bluey_right.xpm"
-# define BALLOON "textures/balloon.xpm"
-# define CACTUS "textures/cactus.xpm"
 # define EXIT_C "textures/exit_close.xpm"
 # define EXIT_O "textures/exit_open.xpm"
 # define TILE "textures/tile.xpm"
 # define WALL "textures/wall.xpm"
+# define CACTUS "textures/cactus.xpm"
+# define BALLOON "textures/balloon.xpm"
 # define WIN "textures/win.xpm"
 
 typedef struct s_mlx
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	char	**map;
-	int		width;
-	int		height;
-	void	*player;
-	int		stars;
-	int		movements;
-	int		col;
-	int		line;
-	int		player_line;
-	int		player_col;
-	void	*bluey_d;
-	void	*bluey_l;
-	void	*bluey_r;
-	void	*bluey_u;
-	void	*exit_c;
-	void	*exit_o;
-	void	*tile;
-	void	*wall;
-	void	*cactus;
-	void	*balloon;
-	void	*win;
+	void		*mlx_ptr;
+	void		*win_ptr;
+	char		**map;
+	int			width;
+	int			height;
+	void		*player;
+	int			stars;
+	int			movements;
+	int			col;
+	int			line;
+	int			player_line;
+	int			player_col;
 }			t_mlx;
 
 typedef struct s_map
